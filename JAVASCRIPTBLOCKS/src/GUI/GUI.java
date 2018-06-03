758c7af37723644030c173ab544d36a870bd8b63
ItIsMagic <morabi.andrea@gmail.com>
2018-06-03 18:13:14 +0200

    GUI debut

---
+++ JAVASCRIPTBLOCKS/src/GUI/GUI.java
@@ -0,0 +1,26 @@
+package GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class GUI extends Application {
+
+    @Override public void start(Stage stage) throws IOException {
+        URL url = getClass().getResource("mainWindow.fxml");
+        FXMLLoader loader = new FXMLLoader(url);
+        BorderPane mainWindow = (BorderPane)loader.load();
+        Scene scene = new Scene(mainWindow);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args){
+        launch(args);
+    }
+}
